d1cfc299822d18c0fb81dd74eca4c9630ff46ac8
Ayush Mazumdar <ayushmz2912@gmail.com>
2024-07-31 19:45:20 +0000

    Removed some redundant code

--- main.go
+++ main.go
@@ -16,13 +16,12 @@ func main() {
 	fmt.Println("Starting server on", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
-		panic(err)
 	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	// why Fprintf instead of Println
+	
+	// sending a message as a http response to the client through the ResponseWriter, using Fprintf
 	fmt.Fprintf(w, "Welcome to the URL shortener service !")
 }
-// what should be the branch name for this commit, if Im planning to have front end and backend part?
 

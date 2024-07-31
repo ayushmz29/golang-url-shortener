8bd4b20cef4160c8e1ab41b0623890c0618ed7df
Ayush Mazumdar <ayushmz2912@gmail.com>
2024-07-31 17:22:19 +0000

    creating the main backend service

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	port = ":8080" // port to run the server on
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	anotherFunction()
+	http.HandleFunc("/", handleRoot)
+
+	fmt.Println("Starting server on", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
 }
 
-func anotherFunction() {
-	fmt.Println("Another function")
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	// why Fprintf instead of Println
+	fmt.Fprintf(w, "Welcome to the URL shortener service !")
 }
+// what should be the branch name for this commit, if Im planning to have front end and backend part?
+
